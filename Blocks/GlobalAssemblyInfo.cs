569c9317286072b7188e0f1e7fc01c5e725b96f2
IharYakimush <IharYakimush@gmail.com>
2017-03-21 17:22:05 +0300

    Update copywright

--- Blocks/GlobalAssemblyInfo.cs
+++ Blocks/GlobalAssemblyInfo.cs
@@ -2,7 +2,7 @@
 // ==============================================================================
 // Microsoft patterns & practices Enterprise Library
 // ==============================================================================
-// Copyright © Microsoft Corporation.  All rights reserved.
+// Copyright © Microsoft Corporation.  All rights reserved. Modifications copyright © 2017 Ihar Yakimush
 // THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
 // OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
 // LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
--- Blocks/SemanticLogging/Src/SemanticLogging.Etw/Configuration/ConfigurationException.cs
+++ Blocks/SemanticLogging/Src/SemanticLogging.Etw/Configuration/ConfigurationException.cs
@@ -3,7 +3,7 @@
 // Microsoft patterns & practices Enterprise Library
 // Semantic Logging Application Block
 // ==============================================================================
-// Copyright © Microsoft Corporation.  All rights reserved.
+// Copyright © Microsoft Corporation.  All rights reserved. Modifications copyright © 2017 Ihar Yakimush
 // THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
 // OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
 // LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
