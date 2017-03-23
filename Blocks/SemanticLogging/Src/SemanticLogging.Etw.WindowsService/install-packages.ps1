a96e1c94bd282c316f36a5b649c4c2831576eed7
IharYakimush <IharYakimush@gmail.com>
2017-03-23 11:49:03 +0300

    nuget

--- Blocks/SemanticLogging/Src/SemanticLogging.Etw.WindowsService/install-packages.ps1
+++ Blocks/SemanticLogging/Src/SemanticLogging.Etw.WindowsService/install-packages.ps1
@@ -8,80 +8,41 @@
 # FITNESS FOR A PARTICULAR PURPOSE.
 # ==============================================================================
 
-param (
-    [switch] $autoAcceptTerms
-)
-
-# list all the solution folders where the "packages" folder will be placed.
-$solutionRelativePaths = @('.nuget')
-
-
 $scriptPath = Split-Path (Get-Variable MyInvocation -Scope 0).Value.MyCommand.Path 
-
-$solutionFolders = New-Object object[] $solutionRelativePaths.Length
-$allPackagesFiles = New-Object object[] $solutionRelativePaths.Length
-for($i=0; $i -lt $solutionRelativePaths.Length; $i++)
-{
-    $solutionFolder = Join-Path $scriptPath $solutionRelativePaths[$i]
-    $solutionFolders[$i] = $solutionFolder
-    $allPackagesFiles[$i] = Get-ChildItem $solutionFolder -Include "packages.config" -Recurse
-}
-
-
-# get all the packages to install
-$packages = @()
-foreach ($packageFilesForSolution in $allPackagesFiles)
-{
-    $packageFilesForSolution | ForEach-Object { 
-        $xml = New-Object "System.Xml.XmlDocument"
-        $xml.Load($_.FullName)
-        $xml | Select-Xml -XPath '//packages/package' | 
-            Foreach { $packages += " " + $_.Node.id + " -version " + $_.Node.version + " " }
-    }
-}
-
-$packages = $packages | Select -uniq | Sort-Object
-$packages = [system.string]::Join("`r`n", $packages)
-
-# prompt to continue
-$caption = "DOWNLOADING NUGET PACKAGE DEPENDENCIES";
-$packageInformation = "You are about to automatically download the following NuGet package dependencies required to run the service:
-" + $packages + "
  
-Microsoft grants you no rights for third party software.  You are responsible for and must locate and read the license terms for each of the above packages. The owners of the above packages are solely responsible for their content and behavior. Microsoft gives no express warranties, guarantees or conditions.
-";
+Write-Host $scriptPath
 
+$repositoryPath = (get-item $scriptPath ).parent.parent
 
-    Write-Host $caption
-    Write-Host $packageInformation
+$packPath = (get-item $scriptPath ).parent
 
-
-# copy NuGet.exe bootstrapper to a temp folder if it's not there (this is to avoid distributing the full version of NuGet, and avoiding source control issues with updates).
-$nuget = Join-Path $scriptPath '.nuget\nuget.exe'
+$contentPath = Join-Path $packPath.FullName 'content'
 
 $env:EnableNuGetPackageRestore=$true
 
-for($i=0; $i -lt $solutionFolders.Length; $i++)
-{
-    pushd $solutionFolders[$i]
+pushd $contentPath
+Get-ChildItem -Recurse | Where { $_.FullName -like "*.xml" } | Copy-Item -Destination $scriptPath
+Get-ChildItem -Recurse | Where { $_.FullName -like "*.xsd" } | Copy-Item -Destination $scriptPath
+popd
 
-    # install the packages
-    $allPackagesFiles[$i] | ForEach-Object { & $nuget install $_.FullName -o packages }
+pushd $repositoryPath
 
     $dependencies = @(
-		'*NET45\Microsoft.Practices.EnterpriseLibrary.SemanticLogging.dll',
-		'*NET45\Microsoft.Practices.EnterpriseLibrary.SemanticLogging.*.dll',
-        '*portable*\Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling.dll',
-        '*NET45\Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling.*.dll',
-        '*\EnterpriseLibrary.SemanticLogging.Database.*\scripts\*.*'
-        '*NET40\Microsoft.Data.Edm.dll',
-        '*NET40\Microsoft.Data.OData.dll',
-        '*NET40\Microsoft.Data.Services.Client.dll',
-        '*Microsoft.WindowsAzure.Configuration.dll',
-        '*NET40\System.Spatial.dll',
-        '*NET40\Microsoft.WindowsAzure.Storage.dll',
-        '*NET40\Newtonsoft.Json.dll'
-        '*net40\Microsoft.Diagnostics.Tracing.TraceEvent.dll'
+		
+		'*\EntLibExtensions.SemanticLogging.1.0.0\lib\net45\*.*',
+		'*\EntLibExtensions.SemanticLogging.WindowsAzure.1.0.0\lib\net45\*.*',
+		'*\EntLibExtensions.SemanticLogging.Database.1.0.0\lib\net45\*.*',		
+		'*\EntLibExtensions.SemanticLogging.Database.1.0.0\content\Scripts\*.*',
+
+		'*\Microsoft.Diagnostics.Tracing.TraceEvent.1.0.41\lib\net40\*.*',
+		'*\Microsoft.Diagnostics.Tracing.EventSource.Redist.1.1.28\lib\net40\*.*',
+		
+		'*\EnterpriseLibrary.TransientFaultHandling.6.0.1304.0\lib\*portable*\*.*',
+		'*\EnterpriseLibrary.TransientFaultHandling.Data.6.0.1304.0\lib\*portable*\*.*',
+
+		'*\WindowsAzure.Storage.2.0.5.1\lib\net40\*.*',		
+		'*\Newtonsoft.Json.4.5.11\lib\net40\*.*'
+
         )
 
     
@@ -91,15 +52,12 @@ for($i=0; $i -lt $solutionFolders.Length; $i++)
     }
 
     popd
-}
 
 Write-Host "
 You can now edit the configuration file to log events from your own application by opening 'SemanticLogging-svc.xml'.
 To get IntelliSense from the XML schema file, you can open the configuration in Microsoft Visual Studio.
-After the configuration is updated, start the Windows service by executing 'SemanticLogging-svc.exe -start' from an elevated command prompt.
+After the configuration is updated, start the Windows service by executing 'SemanticLogging-svc.exe -start' from an elevated command prompt."
 
-Press ENTER key to finish..."
-$x = $host.UI.ReadLine()
 # SIG # Begin signature block
 # MIIatwYJKoZIhvcNAQcCoIIaqDCCGqQCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
