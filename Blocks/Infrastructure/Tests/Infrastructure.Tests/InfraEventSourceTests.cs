a33b971958cfa1920093ce3b878f205d980ad47d
Ihar Yakimush <iharyakimush@gmail.com>
2017-10-20 11:56:20 +0200

    Add unit testing

---
+++ Blocks/Infrastructure/Tests/Infrastructure.Tests/InfraEventSourceTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Infrastructure.Tests
+{
+    using System;
+
+    using EntLibExtensions.Infrastructure;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    [RunInApplicationDomain]
+    public class InfraEventSourceTests
+    {
+        [Test]
+        [RunInApplicationDomain]
+        public void SetName()
+        {
+            string anyname = "anyName";
+
+            InfraEventSource.Initialize(anyname);
+            Assert.AreEqual(anyname, InfraEventSource.Log.Name);
+            Assert.Throws<InvalidOperationException>(() => InfraEventSource.Initialize("anyOtherName"));
+            Assert.Throws<InvalidOperationException>(() => InfraEventSource.Initialize(anyname));
+        }
+    }
+}
--- Blocks/Infrastructure/src/Infrastructure/InfraEventSource.cs
+++ Blocks/Infrastructure/src/Infrastructure/InfraEventSource.cs
@@ -1,7 +1,9 @@
 ﻿namespace EntLibExtensions.Infrastructure
 {
     using System;
+    using System.Diagnostics;
     using System.Security.Principal;
+    using System.Threading;
 
     using Microsoft.Diagnostics.Tracing;
 
@@ -10,43 +12,53 @@
     /// </summary>
     public sealed class InfraEventSource : EventSource
     {
-        private static string name = "EntLibExtensions-Infrastructure";
+        /// <summary>
+        /// The log lazy.
+        /// </summary>
+        private static readonly Lazy<InfraEventSource> LogLazy =
+            new Lazy<InfraEventSource>(() => new InfraEventSource(), true);
 
         /// <summary>
-        /// The initialized.
+        /// The name.
         /// </summary>
-        private static bool initialized;
-
+        private static string name = "EntLibExtensions-Infrastructure";
+        
         /// <summary>
         /// Prevents a default instance of the <see cref="InfraEventSource"/> class from being created.
         /// </summary>
-        private InfraEventSource():base(name)
+        private InfraEventSource() : base(name)
         {
-            initialized = true;
         }
 
         /// <summary>
         /// Gets the log.
         /// </summary>
-        public static InfraEventSource Log { get; } = new InfraEventSource();
+        public static InfraEventSource Log
+        {
+            get
+            {
+                return LogLazy.Value;
+            }
+        }
 
         /// <summary>
         /// The initialize.
         /// </summary>
-        /// <param name="name">
+        /// <param name="sourceName">
         /// The name.
         /// </param>
         /// <exception cref="InvalidOperationException">
+        /// Invalid Operation Exception
         /// </exception>
-        public static void Initialize(string name)
+        public static void Initialize(string sourceName)
         {
-            if (initialized)
+            if (LogLazy.IsValueCreated)
             {
                 throw new InvalidOperationException(
                     "Event source already used. Try to call Initialize method at the very beginning of application lifecycle");
             }
 
-            InfraEventSource.name = name;
+            InfraEventSource.name = sourceName;
         }
 
         /// <summary>
@@ -138,6 +150,142 @@
         }
 
         /// <summary>
+        /// The external process run.
+        /// </summary>
+        /// <param name="processStartInfo">
+        /// The process start info.
+        /// </param>
+        /// <param name="milliseconds">
+        /// Timeout for execution milliseconds
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        [NonEvent]
+        public string ExternalProcessRun(ProcessStartInfo processStartInfo, int milliseconds)
+        {
+            Guid tokenGuid = Guid.NewGuid();
+            string token = tokenGuid.ToString("N").Substring(0, 8);
+            this.ExternalProcessRun(
+                processStartInfo.FileName,
+                token,
+                milliseconds,
+                processStartInfo.UserName,
+                processStartInfo.CreateNoWindow,
+                processStartInfo.LoadUserProfile,
+                processStartInfo.ErrorDialog);
+            return token;
+        }
+
+        /// <summary>
+        /// The external process run.
+        /// </summary>
+        /// <param name="fileName">
+        /// The file name.
+        /// </param>
+        /// <param name="token">
+        /// The token.
+        /// </param>
+        /// <param name="milliseconds">
+        /// The milliseconds.
+        /// </param>
+        /// <param name="userName">
+        /// The user name.
+        /// </param>
+        /// <param name="createNoWindow">
+        /// The create no window.
+        /// </param>
+        /// <param name="loadUserProfile">
+        /// The load user profile.
+        /// </param>
+        /// <param name="errorDialog">
+        /// The error dialog.
+        /// </param>
+        [Event(4, Message = "Run external process {0}. Token {1}", Level = EventLevel.Informational, Keywords = Keywords.Application)]
+        public void ExternalProcessRun(string fileName, string token, int milliseconds, string userName, bool createNoWindow, bool loadUserProfile, bool errorDialog)
+        {
+            this.WriteEvent(4, fileName, token, milliseconds, userName, createNoWindow, loadUserProfile, errorDialog);
+        }
+
+        /// <summary>
+        /// The external process failed to start.
+        /// </summary>
+        /// <param name="fileName">
+        /// The file name.
+        /// </param>
+        /// <param name="token">
+        /// The token.
+        /// </param>
+        /// <param name="errorMessage">
+        /// The error message.
+        /// </param>
+        /// <param name="exception">
+        /// The exception.
+        /// </param>
+        [Event(5, Message = "External process failed {0}. Token {1} Message {2}", Level = EventLevel.Error, Keywords = Keywords.Application)]
+        public void ExternalProcessFailedToStart(string fileName, string token, string errorMessage, string exception)
+        {
+            this.WriteEvent(5, fileName, token, errorMessage, exception);
+        }
+
+        /// <summary>
+        /// The external process timeout.
+        /// </summary>
+        /// <param name="fileName">
+        /// The file name.
+        /// </param>
+        /// <param name="token">
+        /// The token.
+        /// </param>
+        /// <param name="output">
+        /// The output.
+        /// </param>
+        [Event(6, Message = "External process timeout {0}. Token {1}", Level = EventLevel.Warning, Keywords = Keywords.Application)]
+        public void ExternalProcessTimeout(string fileName, string token, string output)
+        {
+            this.WriteEvent(6, fileName, token, output);
+        }
+
+        /// <summary>
+        /// The external process end with error.
+        /// </summary>
+        /// <param name="fileName">
+        /// The file name.
+        /// </param>
+        /// <param name="token">
+        /// The token.
+        /// </param>
+        /// <param name="code">
+        /// The code.
+        /// </param>
+        /// <param name="output">
+        /// The output.
+        /// </param>
+        [Event(7, Message = "External process end {0}. Token {1}. Exit code {2}", Level = EventLevel.Warning, Keywords = Keywords.Application)]
+        public void ExternalProcessEndWithError(string fileName, string token, int code, string output)
+        {
+            this.WriteEvent(7, fileName, token, code, output);
+        }
+
+        /// <summary>
+        /// The external process end.
+        /// </summary>
+        /// <param name="fileName">
+        /// The file name.
+        /// </param>
+        /// <param name="token">
+        /// The token.
+        /// </param>
+        /// <param name="output">
+        /// The output.
+        /// </param>
+        [Event(8, Message = "External process end {0}. Token {1}. Exit code {2}", Level = EventLevel.Informational, Keywords = Keywords.Application)]
+        public void ExternalProcessEnd(string fileName, string token, string output)
+        {
+            this.WriteEvent(8, fileName, token, output);
+        }
+
+        /// <summary>
         /// The key words.
         /// </summary>
         public class Keywords
@@ -160,7 +308,12 @@
             /// <summary>
             /// The action.
             /// </summary>
-            public const EventKeywords WorkerAction = (EventKeywords)8;            
+            public const EventKeywords WorkerAction = (EventKeywords)8;
+
+            /// <summary>
+            /// The external process.
+            /// </summary>
+            public const EventKeywords ExternalProcess = (EventKeywords)16;            
         }
 
         /// <summary>
