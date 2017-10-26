2faf61ddd17cbd4712c47965bf46037c99b52a93
Ihar Yakimush <iharyakimush@gmail.com>
2017-10-26 18:00:32 +0200

    add samples

--- Blocks/Infrastructure/Samples/Infrastructure.Samples/Program.cs
+++ Blocks/Infrastructure/Samples/Infrastructure.Samples/Program.cs
@@ -1,7 +1,5 @@
 ﻿namespace EntLibExtensions.Infrastructure.Samples
 {
-    using System;
-
     using EntLibExtensions.Infrastructure;
     using EntLibExtensions.Infrastructure.Unity;
     using EntLibExtensions.Infrastructure.WinService;
@@ -15,20 +13,28 @@
     {
         static void Main(string[] args)
         {
+            // Subscribe for logging events. Not mandatory, however very useful
             ObservableEventListener listener = new ObservableEventListener();
             listener.EnableEvents(InfraEventSource.Log, EventLevel.LogAlways);
             listener.LogToConsole();
 
+            // Run repeatable actions from configuration as windows service. 
+            // Instances which implements IWorkerAction will be resolved from Unity Container
             Application application = new Application(args);
             application.RunWorkersHostService(
                 strings => new WinServiceOptions("EntLibExtensions.Infrastructure.Samples"),
-                strings =>
-                    {
-                        IUnityContainer container = new UnityContainer();
-                        container.RegisterType<Action1>();
-                        container.RegisterType<Action2>();
-                        return container;
-                    });
+                strings => BootstrapContainer(strings));
+        }
+
+        private static IUnityContainer BootstrapContainer(string[] strings)
+        {
+            IUnityContainer container = new UnityContainer();
+
+            // Actions have constructor without parameters, so simply register them in container
+            container.RegisterType<Action1>();
+            container.RegisterType<Action2>();
+
+            return container;
         }
     }
 }
